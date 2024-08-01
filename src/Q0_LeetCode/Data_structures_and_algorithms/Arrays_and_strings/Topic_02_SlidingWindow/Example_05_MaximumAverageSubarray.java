70c8927e0e1ca677066e417b350d977b7c16de72
pravrav <naveen.revalino@gmail.com>
2024-08-01 13:00:59 -0400

    #11 Completed section 2 : updating the comments and cleaning the code.

--- src/Q0_LeetCode/Data_structures_and_algorithms/Arrays_and_strings/Topic_02_SlidingWindow/Example_05_MaximumAverageSubarray.java
+++ src/Q0_LeetCode/Data_structures_and_algorithms/Arrays_and_strings/Topic_02_SlidingWindow/Example_05_MaximumAverageSubarray.java
@@ -21,19 +21,36 @@ public class Example_05_MaximumAverageSubarray {
         System.out.println( finalResult );
     }
 
-    public static double getMaximumAverage( int[] numbers, int fixedWindowSize ){
+    /*
+        TODO SUDO CODE
+        1. FOR-LOOP - To build a subarray from index(0) up to index( fixed window size -1 )
+            On Each Iteration,
+            1.1: Add each element in the subarray, save the total sum to currentSum.
+        2. Calculate the average of the sum and save it to currentAverage variable.
+        3. FOR-LOOP - To slide the fixed window over the original array to find all other subarrays.
+                      This loop will start from the next index position, which is the value of fixedWindowSize variable.
+            3.1: Calculate the sum of all elements of this new subarray / window, There is an easy way to do this.
 
-        /*
-        SUDO CODE
-        1. Using a FOR-LOOP generate the fixed window , inside calculate sum of all elements.
-        2. Calculate the average by dividing the sum by the fixedWindowSize.
-        3. Using a FOR-LOOP slide the fixed window over the array to find sum and average of each possible subarray.
-        4. Return the maximumAverage.
-         */
+                 Imagine given array is : [ 1 2 3 4 5 6 7 8 ], and the first subarray starts from index(0) till index(3).
+                 Then Sum = 1 + 2 + 3 + 4 = 10.
+                 Now we are moving the window, the new subarray will start from index(1) and end at index(4).
+                 Then Sum = 2 + 3 + 4 + 5 = 14.
+
+                 the common element between the first subarray and the second subarray are 2, 3, and 4, the elements that
+                 changed are 1, which was removed and 5 was added, hence from the previous subarray Sum 10, reduce 1, and
+                 add 5 to get the new sum 10 -1 + 5 = 14.
+            3.2: Calculate the new average, keep track of the highest average.
+        4. Return the highest average.
+     *
+     * @param numbers : An integer array ( with positive and negative numbers )
+     * @param fixedWindowSize : An integer denoting the fixed window / subarray size.
+     * @return An integer, which is the Maximum average value of a contiguous subarray whose length is equal to fixedWindowSize( K ).
+     */
+    public static double getMaximumAverage( int[] numbers, int fixedWindowSize ){
 
         // Method Variable(s)
         double currentSum = 0.00;
-        double currentAverage = 0.00;
+        double currentAverage;
 
         for( int rightBound = 0; rightBound < fixedWindowSize; rightBound ++ ){
             currentSum += numbers[rightBound];
@@ -45,7 +62,6 @@ public class Example_05_MaximumAverageSubarray {
             currentSum += numbers[leftBound] - numbers[ leftBound - fixedWindowSize ];
             currentAverage = Math.max( currentAverage, currentSum / fixedWindowSize );
         }
-
 
         return currentAverage;
     }
